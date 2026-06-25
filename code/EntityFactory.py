0e767d01a70e025b3b22b55c41fe71535ff73d2d
Ryan Martins <ryanbarroso2017@gmail.com>
2026-06-25 14:45:47 -0300

    feat(level): Support for Level 2 and 3, enemy and player entities

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,5 +1,7 @@
 from code.Background import Background
 from code.Const import WINDOW_WIDTH
+from code.Enemy import Enemy
+from code.Player import Player
 
 
 class EntityFactory:
@@ -14,5 +16,29 @@ class EntityFactory:
                     for pos in [position, (WINDOW_WIDTH, 0)]
                 ]
                 return list_bg
-        return None
 
+            case "Level02Bg":
+                list_bg = [
+                    Background(f"Level02Bg{i}", pos)
+                    for i in range(4)
+                    # Gera 2 Backgrounds, um no início e outro no fim da tela
+                    for pos in [position, (WINDOW_WIDTH, 0)]
+                ]
+                return list_bg
+
+            case "Level03Bg":
+                list_bg = [
+                    Background(f"Level03Bg{i}", pos)
+                    for i in range(4)
+                    # Gera 2 Backgrounds, um no início e outro no fim da tela
+                    for pos in [position, (WINDOW_WIDTH, 0)]
+                ]
+                return list_bg
+
+            case "Player":
+                return Player("Player", position)
+
+            case "Enemy":
+                return Enemy("Enemy", position)
+
+        return None
