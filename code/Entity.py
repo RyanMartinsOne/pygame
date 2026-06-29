f879bce7df60451afe163dca44a8ecd0d191d40d
Ryan Martins <ryanbarroso2017@gmail.com>
2026-06-29 11:31:39 -0300

    feat(Entity): Add mask for collisions, function load_image for load entities images and set_image function for sincronize image and mask

--- code/Entity.py
+++ code/Entity.py
@@ -6,9 +6,19 @@ import pygame.image
 class Entity(ABC):
     def __init__(self, name:str, position:tuple):
         self.name = name
-        self.image = pygame.image.load('./assets/images/' + name + '.png').convert_alpha()
+        self.image = Entity.load_image(name)
         self.rect = self.image.get_rect(left=position[0], top=position[1])
+        self.mask = pygame.mask.from_surface(self.image)
         self.speed = 0
+
+    @staticmethod
+    def load_image(name: str) -> pygame.Surface:
+        return pygame.image.load(f'./assets/images/{name}.png').convert_alpha()
+
+    def set_image(self, surface:pygame.Surface, topleft:tuple):
+        self.image = surface
+        self.rect = self.image.get_rect(topleft=topleft)
+        self.mask = pygame.mask.from_surface(self.image)
 
     @abstractmethod
     def move(self):
